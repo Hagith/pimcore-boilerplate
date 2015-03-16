5d13367beb8e97c080fcbbdafa4edde48af65673
Rafal Galka <r.galka@samsung.com>
2015-03-16 13:13:36 +0100

    [gulp] log error stack to console

--- .gulp/utils/handleErrors.js
+++ .gulp/utils/handleErrors.js
@@ -1,7 +1,9 @@
 var notify = require('gulp-notify');
+var gutil = require('gulp-util');
 
 module.exports = function(error) {
-  notify.onError(error.stack).apply(this, arguments);
+  notify.onError(error.toString()).apply(this, arguments);
+  gutil.log(error.stack);
   // Keep gulp from hanging on this task
   this.emit('end');
 };
